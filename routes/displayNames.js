4ba724a650aeb4087070e5d114ad6c66921c8214
Jorge Rodriguez <102831018+JorgeRSLC@users.noreply.github.com>
2024-02-16 15:51:30 -0700

    Build functionality to display names from db in main view

--- routes/displayNames.js
+++ routes/displayNames.js
@@ -1,8 +1,18 @@
-module.exports = (req, res) => {
-  res.render('allNamesView', { names: [
-    { name: 'John Lennon' },
-    { name: 'Paul McCartney' },
-    { name: 'George Harrison' },
-    { name: 'Ringo Star' }
-  ] });
+// import name models 
+const firstNamesModel = require('../models/firstNameModel');
+const lastNamesModel = require('../models/lastNameModel');
+
+module.exports = async (req, res) => {
+
+  const firstNames = await firstNamesModel.find({});
+  const lastNames = await lastNamesModel.find({});
+
+  const fullNames = [];
+
+  for (let i = 0; i < Math.min(firstNames.length, lastNames.length); i++) {    
+    const fullName = `${firstNames[i].firstName} ${lastNames[i].lastName}`;
+    fullNames.push({'name':fullName});
+}
+  
+  res.render('allNamesView', { names: fullNames });
 }
