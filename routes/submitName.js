383f0207ef4040239b23d0763af485da46adb30d
Jorge Rodriguez <102831018+JorgeRSLC@users.noreply.github.com>
2024-02-16 17:43:47 -0700

    Build add-name and submit-name functionality
    First name and last name saved to two seperate collections on remote db

--- routes/submitName.js
+++ routes/submitName.js
@@ -1,3 +1,20 @@
 module.exports = (req, res) => {
+    // import name models 
+    const firstNamesModel = require('../models/firstNameModel');
+    const lastNamesModel = require('../models/lastNameModel');
+
+    // get the first name and last name from the request
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+
+    // create a new first name and last name model
+    const newFirstName = new firstNamesModel({ firstName: firstName });
+    const newLastName = new lastNamesModel({ lastName: lastName });
+
+    // save the new first name and last name
+    newFirstName.save();
+    newLastName.save();
+
+    // redirect to the displayNames route
     res.redirect('/displayNames');
 }
